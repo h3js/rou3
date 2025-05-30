135aec98bd4ddc440af0efa8351a5886b3f1602c
Pooya Parsa <pooya@pi0.io>
2025-05-30 11:52:41 +0200

    feat: `routeToRegExp` util (#153)

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,4 @@ export { addRoute } from "./operations/add.ts";
 export { findRoute } from "./operations/find.ts";
 export { removeRoute } from "./operations/remove.ts";
 export { findAllRoutes } from "./operations/find-all.ts";
+export { routeToRegExp } from "./regexp.ts";
