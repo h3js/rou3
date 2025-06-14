897356b48e6c52c954b4bbf0bd9d96f116a52757
aquapi <aquaplmc@gmail.com>
2025-06-14 20:33:47 +0700

    fix: wrong symbol name for path input

--- src/operations/compile.ts
+++ src/operations/compile.ts
@@ -206,6 +206,6 @@ export const compileRoute = <T>(
 
   return new Function(
     ...deps.map((_, i) => "d" + (i + 1)),
-    `return(p,m)=>{let l=path.length;${compiled}}`,
+    `return(p,m)=>{let l=p.length;${compiled}}`,
   )(...deps);
 };
