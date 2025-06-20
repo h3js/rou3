e848ec791fabfc902659f5db2040261e4266f22f
Pooya Parsa <pooya@pi0.io>
2025-06-20 11:44:20 +0200

    test: update regexp tests

--- test/regexp.test.ts
+++ test/regexp.test.ts
@@ -1,34 +1,61 @@
 import { describe, it, expect } from "vitest";
-import { routeToRegExp } from "../src/index.ts";
+import {
+  routeToRegExp,
+  createRouter,
+  addRoute,
+  findRoute,
+} from "../src/index.ts";
 
 describe("routeToRegExp", () => {
-  const cases = {
-    "/path": { regex: /^\/path\/?$/, match: ["/path"] },
+  const routes = {
+    "/path": { regex: /^\/path\/?$/, match: [["/path"], ["/path/"]] },
     "/path/:param": {
       regex: /^\/path\/(?<param>[^/]+)\/?$/,
-      match: ["/path/value"],
+      match: [
+        ["/path/value", { param: "value" }],
+        ["/path/value/", { param: "value" }],
+      ],
     },
-    "/path/get-:file:ext.txt": {
-      regex: /^\/path\/get-(?<file>[^/]+)(?<ext>[^/]+)\.txt\/?$/,
-      match: ["/path/get-file.ext.txt"],
+    "/path/get-:file.:ext": {
+      regex: /^\/path\/get-(?<file>[^/]+)\.(?<ext>[^/]+)\/?$/,
+      match: [["/path/get-file.txt", { file: "file", ext: "txt" }]],
     },
     "/path/:param1/:param2": {
       regex: /^\/path\/(?<param1>[^/]+)\/(?<param2>[^/]+)\/?$/,
-      match: ["/path/value1/value2"],
+      match: [["/path/value1/value2", { param1: "value1", param2: "value2" }]],
     },
     "/path/*/foo": {
       regex: /^\/path\/[^/]*\/foo\/?$/,
-      match: ["/path/anything/foo"],
+      match: [["/path/anything/foo"], ["/path//foo"], ["/path//foo/"]],
     },
-    "/path/**": { regex: /^\/path\/.*\/?$/, match: ["/path/anything/more"] },
-  };
-  for (const [route, expected] of Object.entries(cases)) {
+    "/path/**": {
+      regex: /^\/path\/.*\/?$/,
+      match: [
+        ["/path/"],
+        // ["/path"], // TODO
+        ["/path/anything/more"],
+      ],
+    },
+  } as const;
+
+  for (const [route, expected] of Object.entries(routes)) {
     it(`should convert route "${route}" to regex "${expected.regex.source}"`, () => {
+      const router = createRouter();
+      addRoute(router, "", route, { route });
+
       const regex = routeToRegExp(route);
-      expect(regex.source).toBe(expected.regex.source);
-      for (const match of expected.match) {
-        expect(regex.test(match), `Matches ${match}`).toBe(true);
+
+      for (const [path, params] of expected.match) {
+        expect(findRoute(router, "", path)).toMatchObject({ data: { route } });
+
+        const match = path.match(regex);
+        expect(match, path).not.toBeNull();
+        if (params) {
+          expect(match?.groups).toMatchObject(params);
+        }
       }
+
+      expect(regex.source).toBe(expected.regex.source);
     });
   }
 });
