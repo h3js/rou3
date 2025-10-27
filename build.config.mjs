425ee50c9c0de817fb02edc4b442dedabe98b9e8
Pooya Parsa <pooya@pi0.io>
2025-10-27 12:35:09 +0100

    build: reduce dist size

--- build.config.mjs
+++ build.config.mjs
@@ -2,4 +2,10 @@ import { defineBuildConfig } from "obuild/config";
 
 export default defineBuildConfig({
   entries: ["src/index.ts", "src/compiler.ts"],
+  hooks: {
+    rolldownConfig(config) {
+      config.experimental ??= {};
+      config.experimental.attachDebugInfo = "none";
+    },
+  },
 });
